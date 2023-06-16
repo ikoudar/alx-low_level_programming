8de7cfc500ccc40ae2a30d6bf816fd20d332f43b
ikoudar <koudarimad@gmail.com>
2023-06-16 09:25:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
-*main - print the string in put function
-*description: using the main fucntion
+* main - print the string in the put function
+* Description: using the main function
 */
-int main(avoid)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
