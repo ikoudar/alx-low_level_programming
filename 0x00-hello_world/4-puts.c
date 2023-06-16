31da194045376c3489ee8f2b51f8b4784a9175e1
ikoudar <koudarimad@gmail.com>
2023-06-16 09:05:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+/*
+*main - print the string in put function
+*description: using the main fucntion
+*/
 int main(avoid)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
