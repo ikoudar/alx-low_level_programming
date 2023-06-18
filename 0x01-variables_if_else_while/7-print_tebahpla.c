cf59293e77004125233ba17e37d2fd65727db68f
ikoudar <koudarimad@gmail.com>
2023-06-18 14:29:41 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point to the program
+ *
+ * Return: 0 on successful execution
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
