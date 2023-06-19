40b4ce4137d6fadc816d42eab9f9903a6160b786
ikoudar <koudarimad@gmail.com>
2023-06-19 15:21:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-/**
- * main - entry point to the program
- *
- * Return: 1 on successful execution
-*/
-int main(void)
-{
-	const char = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	{
-		putchar('\n');
-	}
-	return (1)
-}
+#!/bin/bash
+gcc -Wall program.c -o program
