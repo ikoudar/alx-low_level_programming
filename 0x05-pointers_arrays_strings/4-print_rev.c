3166580872609862072268569d8071c4d1bff564
ikoudar <koudarimad&gmail.com>
2023-06-26 17:39:03 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *print_rev - print string in reverse, following by new line.
+ *
+ *@s: pointer into teh string
+*/
+
+void print_rev(char *s)
+{
+	revstr(str);
+
+	_putchar(str);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 int _putchar(char c);
 
+int main(void);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
