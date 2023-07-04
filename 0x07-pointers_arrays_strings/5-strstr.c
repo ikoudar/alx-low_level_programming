7dcce0d4329cc896f3835ffee09fab713d5dd074
ikoudar <koudarimad&gmail.com>
2023-07-04 11:33:28 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,11 +3,36 @@
 
 /**
   *_strstr- function that locates a substring.
-  *
-  *
+  *@haystack:string
+  *@needle: substring
   *Return: pointer to the beginning of the located substring.
 */
 
 char *_strstr(char *haystack, char *needle)
 {
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n && *h && *n == *h)
+		{
+			n++;
+			h++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+
+	return (NULL);
+}
