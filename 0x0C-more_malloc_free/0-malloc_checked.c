7f3279d0c9c8cecafc248626615553bad6ce853a
ikoudar <koudarimad&gmail.com>
2023-07-15 17:02:46 -0700

    0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
   *malloc_checked- function that allocates memory.
@@ -13,17 +11,11 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
 
+	ptr = malloc(b);
 	if (ptr == NULL)
 	{
-		const char *msg = "memory allocation failed\n";
-
-		while (*msg)
-		{
-			putchar(*msg++);
-		}
-
 		exit(98);
 	}
 	return (ptr);
