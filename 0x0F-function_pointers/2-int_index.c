e8c98d3b68c9d18fc880a9f24038c8286207ba0b
ikoudar <koudarimad&gmail.com>
2023-07-20 11:09:56 -0700

    2-int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *int_index- .....
+  *@cmp: .....
+  *@size: ......
+  *@array: ....
+  *Return: -1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
