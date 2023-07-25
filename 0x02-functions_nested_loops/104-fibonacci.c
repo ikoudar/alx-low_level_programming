d2b328852ce2631e6e17f5c93cd4476c82263bfb
ikoudar <koudarimad@gmail.com>
2023-07-25 05:37:50 -0700

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  *main- the entry point to the program.
+  *
+  *fibonacci suit numbers.
+  *Return: 0
+*/
+
+int main(void)
+{
+	int i = 0;
+	unsigned long a = 0;
+	unsigned long b = 1;
+	unsigned long fib;
+
+	for (i <= 97)
+	{
+		fib = a + b;
+		if (i <= 97)
+		{
+			printf("%lu, ", fib);
+		}
+		else
+		{
+			printf("%lu", fib);
+		}
+		a = b;
+		b = fib;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
