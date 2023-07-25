503a0f1a44e6f9679eb89ff7a9ddfebcf75fdac3
ikoudar <koudarimad@gmail.com>
2023-07-25 05:29:13 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main- the entry point to the program.
+  *
+  *fibonacci suit numbers.
+  *Return: 0.
+*/
+
+int main(void)
+{
+	unsigned long a = 0, b = 1, c = 0, sum = 0;
+
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if ((a % 2) == 0)
+			sum += 1;
+	}
+
+	printf("%ld/n", sum);
+	return (0);
+}
