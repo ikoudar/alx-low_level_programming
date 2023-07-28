70f1892ddb1c871c3255b175a881d19fdbb86f2d
ikoudar <koudarimad@gmail.com>
2023-07-28 09:58:38 -0700

    0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *print_list- function that prints all the elements of a list.
+  *@h: head of th list
+  *
+  *
+  *Return: j
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%lu] %s\n", j, h->str);
+		}
+		h = h->next;
+		j++;
+	}
+	return (j);
+}
