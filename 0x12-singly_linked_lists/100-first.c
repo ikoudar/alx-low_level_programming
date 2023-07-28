5eff7d6787902b2a767e467d9e4d912c562df8c0
ikoudar <koudarimad@gmail.com>
2023-07-28 15:59:24 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  *print_line- function that prints line.
+  *
+  *
+  *
+*/
+
+void print_line(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
