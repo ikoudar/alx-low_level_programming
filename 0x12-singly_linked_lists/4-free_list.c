42382ddfefc9a44ce6132aa2b0adc4e1704a8054
ikoudar <koudarimad@gmail.com>
2023-07-28 17:34:11 -0700

    4-free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,15 +13,12 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
-	
-	if (head != NULL)
+
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			temp = head;
-			head = head->next;
-			free(temp->str);
-			free(temp);
-		}
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
 }
