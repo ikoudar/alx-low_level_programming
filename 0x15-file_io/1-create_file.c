37268f8b186f4e019208cc58d08af695541f37e1
ikoudar <koudarimad@gmail.com>
2023-08-08 07:08:06 -0700

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,10 +3,10 @@
 
 /**
   *create_file-  function that creates a file.
+  *@filename: the name of the file
+  *@text_content: content of the file
   *
-  *
-  *
-  *
+  *Return: 1
 */
 
 int create_file(const char *filename, char *text_content)
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1 || wrote == -1)
 		return (-1);
 
-	close (file);
+	close(file);
 
 	return (1);
 }
