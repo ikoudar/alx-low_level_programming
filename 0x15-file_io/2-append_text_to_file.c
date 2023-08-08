a6d24c672d8183ab6b27270739b6b3b8e4430831
ikoudar <koudarimad@gmail.com>
2023-08-08 07:12:17 -0700

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,9 +4,9 @@
 /**
   *append_text_to_file- function that appends text at the end of a file.
   *
-  *
-  *
-  *
+  *@filename: name of the file
+  *@text_content: content of the file
+  *Return: 1
 */
 
 int append_text_to_file(const char *filename, char *text_content)
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1 || wrote == -1)
 		return (-1);
 
-	close (0);
+	close(0);
 
 	return (1);
 }
