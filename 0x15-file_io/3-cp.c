1721147d6bfd22226b9dc76eee1baf068a3c4104
ikoudar <koudarimad@gmail.com>
2023-08-09 07:35:08 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,47 @@
 void new_buffer(int stat, int fd, char *filename, char file);
 
 /**
+ *main- the entry point to the program
+ *@argc: argument count
+ *@argv: argument passed
+ *Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+
+	int src, dest, buf_read = 1024, wrote, close_src, close_dest;
+	unsigned int files = 0644;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src = open(argv[1], O_RDONLY);
+	new_buffer(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, files);
+	new_buffer(dest, -1, argv[2], 'W');
+	while (buf_read == 1024)
+	{
+		buf_read = read(src, buffer, sizeof(buffer));
+		if (buf_read == -1)
+			new_buffer(-1, -1, argv[1], 'O');
+
+		wrote = write(dest, buffer, buf_read);
+			if (wrote == -1)
+				new_buffer(-1, -1, argv[2], 'W');
+	}
+	close_src = close(src);
+	new_buffer(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	new_buffer(close_dest, dest, NULL, 'C');
+
+	return (0);
+}
+
+/**
   *new_buffer- check the statut of the file.
   *@stat: file to be open
   *@fd: file
@@ -16,7 +57,6 @@ void new_buffer(int stat, int fd, char *filename, char file);
 
 void new_buffer(int stat, int fd, char *filename, char file)
 {
-	int C;
 
 	if (file == 'C' && stat == -1)
 	{
@@ -31,48 +71,7 @@ void new_buffer(int stat, int fd, char *filename, char file)
 	}
 	else if (file == 'W' && stat == -1)
 	{
-		drpintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
-}
-
-/**
-  *main- the entry point to the program
-  *@argc: argument count
-  *@argv: argument passed
-  *Return: 0
-*/
-
-int main(int argc, char *argv[])
-{
-	int src, dest, buf_read = 1024, wrote, close_src, close_dest;
-	unsigned int file;
-	char buffer[1024];
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	src = open(argv[2], O_RDONLY);
-	new_buffer(src, -1, argv[1], '0');
-	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file);
-	new_buffer(dest, -1, argv[2], 'W');
-
-	while (buf_read == 1024)
-	{
-		buf_read = read(src, buffer, sizeof(buffer));
-		if (buf_read == -1)
-			new_buffer(-1, -1, argv[1], '0');
-		worte = write(dest, buffer, argv[2], buf_read)
-			if (wrote == -1)
-				new_buffer(-1, -1, argv[2], 'W');
-	}
-	close_src = close(src);
-	new_buffer(close_src, src, NULL, 'C');
-	close_dest = close(dest);
-	new_buffer(close_dest, dest, NULL, 'C');
-
-	return (0);
 }
